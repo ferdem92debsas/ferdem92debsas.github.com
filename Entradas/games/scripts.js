1782007473da168ab81a7fa2501092f4b8366472
ferdem92debsas <79490598+ferdem92debsas@users.noreply.github.com>
2021-11-07 02:32:50 -0300

    Agrego el proyecto Entradas
    "Entradas" es una galeria con un ranking games.

---
+++ Entradas/games/scripts.js
@@ -0,0 +1,22 @@
+let link = document.querySelectorAll(".close");
+
+link.forEach(function(links){
+links.addEventListener("click",function(ev){
+//    ev.preventDefault();
+
+    let content_games = document.querySelector(".content_games");
+
+    content_games.classList.remove("animate__animated");
+    content_games.classList.remove("animate__pulse");
+    content_games.classList.add("animate__animated");
+    content_games.classList.add("animate__headShake");
+    
+    setTimeout(function(){
+       // window.close();
+        location.href= "../index.html";
+    },700);
+
+    return false;
+});
+
+});

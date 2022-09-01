3236afee0cef024fae86f4823b43911c7108be7e
ferdem92debsas <79490598+ferdem92debsas@users.noreply.github.com>
2022-09-01 12:42:10 -0300

    rename & retokes en la página
    para promociones mediante wsp

--- reparacionesfernando/galeriaV.js
+++ reparacionesfernando/galeriaV.js
@@ -8,10 +8,10 @@ class Galeria {
     }
 }
 
-const cuadro = new Galeria (
+const memoria8gb = new Galeria (
     '<img src="./Accesorios/memoria_ddr3_8gb_corsair.png" alt="">',
     'memoria%20ddr3%20corsair%208gb',
-    'https://articulo.mercadolibre.com.ar/MLA-1102786269-memoria-ram-ddr3-8gb-1600mhz-corsair-vengeance-low-profile-_JM#position=20&search_layout=grid&type=item&tracking_id=66e880ed-2e3e-4c59-a776-9693a6f59fc4',
+    'https://www.mercadolibre.com.ar/memoria-ram-vengeance-lp-gamer-color-black-8gb-1-corsair-cml8gx3m1a1600c10/p/MLA14729174?pdp_filters=category:MLA1648#searchVariation=MLA14729174&position=1&search_layout=stack&type=product&tracking_id=338f3f04-7bdf-420a-8a5b-adbe0b10f49a',
     'memoria8gb',
     `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20memoria%20ddr3%20corsair%208gb`
 );
@@ -19,14 +19,14 @@ const fuente = new Galeria(
     '<img src="./atx.jpg" alt="" srcset="">',
     'fuente%20atx',
     './index.html',
-    'fuente500w',
+    'fuente',
     `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20fuente%20atx`
 )
 const memoriaKingston = new Galeria(
     '<img src="./Accesorios/memoria_ddr3_8gb_kingston.png" alt="" srcset="">',
     'memoria%20kingston%208%20gb%20',
     'https://articulo.mercadolibre.com.ar/MLA-922793154-memoria-kingston-8gb-ddr3-1600-cl11-pc-kvr16n118wp-150v-_JM#position=17&search_layout=grid&type=item&tracking_id=a101a4cf-1e83-41a8-b5c3-7fb3e1410db1',
-    'kington8',
+    'memoriaKingston',
     `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20memoria%20kingston%208%20gb%20`
 )
 const fuente800 = new Galeria(
@@ -40,7 +40,7 @@ const wd3Elements = new Galeria(
     '<img src="./Accesorios/wdElementsExterno.png" alt="" srcset="">',
     'disco%20externo%201%20tera',
     'https://www.mercadolibre.com.ar/disco-duro-externo-western-digital-wd-elements-wdbuzg0010bbk-1tb-negro/p/MLA6326753?pdp_filters=category:MLA1672#searchVariation=MLA6326753&position=1&search_layout=stack&type=product&tracking_id=c4a81c00-5612-4497-b262-b921f45519df',
-    'discoExterno',
+    'wd3Elements',
     `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20disco%20externo%201%20tera`    
     
 ) 
@@ -55,14 +55,14 @@ const vinchaRedragon = new Galeria(
     ' <img src="./Accesorios/auriculares_vincha_redragon.png" alt="" srcset="">',
     'auriculares%20Redragon%20Lamia%20RGB%20con%20luz%20rgb',
     'https://www.mercadolibre.com.ar/auriculares-gamer-redragon-lamia-rgb-negro-con-luz-rgb/p/MLA15558829?pdp_filters=category:MLA6049#searchVariation=MLA15558829&position=18&search_layout=stack&type=product&tracking_id=bde312d1-40d6-4648-8493-b6be1518ebc0',
-    'auricularRedragon',
+    'vinchaRedragon',
     `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20auriculares%20Redragon%20Lamia%20RGB%20con%20luz%20rgb`    
    ) 
 const motherAsus = new Galeria(
     '<img src="./Accesorios/Motherboard_Asus_Prime_A320m-k-Am4_Ddr4_A320_Hdmi_M2 (1).png" alt="" srcset="">',
     'Motherboard%20Asus%20Prime%20Am4%20Ddr4',
     'https://articulo.mercadolibre.com.ar/MLA-1112205731-motherboard-asus-prime-a320m-k-am4-ddr4-a320-hdmi-m2-_JM?searchVariation=173905089285#searchVariation=173905089285&position=49&search_layout=grid&type=item&tracking_id=35aca873-ca78-45dd-a131-38b3a2f34782',
-    'motherAsusAm4',
+    'motherAsus',
     `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20Motherboard%20Asus%20Prime%20Am4%20Ddr4`    
   
 )
@@ -95,6 +95,35 @@ const joystickLogitech = new Galeria(
       'fuente550wLNZ',
       `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20fuente%20ATX%20550wLNZ`
   )
+  const disco320 = new Galeria(
+    '<img src="./Accesorios/disco_rigido320.png" alt="" srcset="">',
+    'disco%20rigido%20toshiba%20320gb',
+    'https://articulo.mercadolibre.com.ar/MLA-746361718-disco-rigido-320gb-toshiba-sata-notebook-netbook-mac-ps3-dvr-_JM?searchVariation=80967899069#searchVariation=80967899069&position=4&search_layout=stack&type=item&tracking_id=b5715464-8e23-4c72-9d01-ffe41240b823',
+    'disco320',
+    `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20disco%20rigido%20toshiba%20320gb`
+  )
+    const adaptadorSata= new Galeria(
+    '<img src="./Accesorios/adaptador_sata_usb.png" alt="" srcset="">',
+    'adaptador%20sata%20usb',
+    'https://articulo.mercadolibre.com.ar/MLA-877647668-cable-adaptador-usb-30-a-sata-25-discos-rigidos-y-grabador-_JM#position=2&search_layout=stack&type=pad&tracking_id=8cbb63f1-1d11-436a-9649-2c402aab2f83#position=2&search_layout=stack&type=pad&tracking_id=8cbb63f1-1d11-436a-9649-2c402aab2f83&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=2&ad_click_id=OTNjMjRhMDUtMTMyOC00N2I3LWIyZTMtYzZkNjFjZjI2Mjdm',
+    'adaptadorSata',
+    `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20adaptador%20sata%20usb`
+    )
+    const auricularesBluetooth= new Galeria(
+        '<img src="./Accesorios/Auriculares Bluetooth Inalambricos Deportivo Pixpro Sm100.png" alt="" srcset="">',
+        'auriculares%20bluethooth%20deportivo',
+        'https://articulo.mercadolibre.com.ar/MLA-1145152092-auriculares-bluetooth-inalambricos-deportivo-pixpro-sm100-_JM#position=3&search_layout=stack&type=item&tracking_id=c03f7598-3a65-45ab-aaf2-757f0988f60c',
+        'auricularesBluetooth',
+        `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20auriculares%20bluethooth%20deportivo`
+    )
+    const conversorHdmiVga= new Galeria(
+        ' <img src="./Accesorios/adaptador_hdmi_vga.png" alt="" srcset="">',
+        'conversor%20hdmi-vga',
+        'https://articulo.mercadolibre.com.ar/MLA-1114163236-cable-conversor-hdmi-a-vga-video-proyector-1080p-hd-_JM#position=5&search_layout=grid&type=item&tracking_id=d15ae82a-6478-4732-abe6-4332d3372576',
+        'conversorHdmiVga',
+        `http://wa.me/541134487550/?text=me%20interesa%20el%20articulo=%20conversor%20hdmi-vga`
+    )
+
   let items = document.getElementById('items');
   /*var mql = window.matchMedia('(max-width: 600px)');
   mql.addEventListener('change',function(e){
@@ -115,31 +144,39 @@ const itemImg = (cuadro)=> {
 
 document.addEventListener("DOMContentLoaded",function(e){
     e.preventDefault; 
-    let marco = itemImg(cuadro);
-    
-    
-    items.innerHTML = marco;
+   // let marco = itemImg(cuadro);
+   items.innerHTML += itemImg(conversorHdmiVga); 
+   items.innerHTML += itemImg(auricularesBluetooth); 
+   items.innerHTML += itemImg(adaptadorSata);
+   items.innerHTML += itemImg(fuente550wLNZ);
+   items.innerHTML += itemImg(disco320);
+   items.innerHTML += itemImg(memoria8gb);
     items.innerHTML += itemImg(wd3Elements);
     items.innerHTML += itemImg(vinchaRedragon);
     items.innerHTML += itemImg(joystickLogitech);
     items.innerHTML += itemImg(memoria4gbSentey);
-    items.innerHTML += itemImg(fuente550wLNZ);
-   
-
-    let memoria = document.getElementById('memoria8gb');
+    
+    let conversor_hdmi_vga = document.getElementById(conversorHdmiVga.id);
+    let auricularBluetooth = document.getElementById(auricularesBluetooth.id);
+    let adaptador = document.getElementById(adaptadorSata.id);
+    let fuente550w = document.getElementById(fuente550wLNZ.id);
+    let discoNet = document.getElementById(disco320.id);
+    let memoria = document.getElementById(memoria8gb.id);
     let disco1Tera = document.getElementById(wd3Elements.id);
     let auricularRedragon = document.getElementById(vinchaRedragon.id);
     let joystickLogitec = document.getElementById(joystickLogitech.id);
     let memoria4gb1600Mhz = document.getElementById(memoria4gbSentey.id);
-    let fuente550w = document.getElementById(fuente550wLNZ.id);
   
-    
-    md_link(memoria,cuadro);
+    md_link(conversor_hdmi_vga,conversorHdmiVga);
+    md_link(auricularBluetooth,auricularesBluetooth);
+    md_link(adaptador,adaptadorSata);
+    md_link(fuente550w,fuente550wLNZ);
+    md_link(discoNet,disco320);
+    md_link(memoria,memoria8gb);
     md_link(disco1Tera,wd3Elements);
     md_link(auricularRedragon,vinchaRedragon);
     md_link(joystickLogitec,joystickLogitech);
     md_link(memoria4gb1600Mhz,memoria4gbSentey);
-    md_link(fuente550w,fuente550wLNZ);
 
     
     },true);
@@ -154,7 +191,7 @@ const md_link = (mem,o)=>{
 const rollers = document.getElementById('rollers');
 
 const imgVec =[
-    cuadro,fuente800,joystickLogitech,grafica1gb,motherAsus,vinchaRedragon,wd3Elements,
+    memoria8gb,fuente800,joystickLogitech,grafica1gb,motherAsus,vinchaRedragon,wd3Elements,
     memoria4gbSentey,fuente550wLNZ
 ]
 
